cba5a42e9c1e0b06ecd6ee6c27cbf67df91b0601
Ayman Farhat <aymanf@google.com>
2022-01-17 21:28:49 +0100

    Upgrade dev dependencies including webpack(4 to 5) to resolve security issues

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, 'dist'),
     port: 4000
   },
   devtool: 'source-map'
